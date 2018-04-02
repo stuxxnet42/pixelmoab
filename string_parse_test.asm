a6b5b015dcf0477bcf6ab06bd25f061025419bbd
monad <mschwar42@gmail.com>
2018-04-02 15:51:41 +0200

    additional comments

--- string_parse_test.asm
+++ string_parse_test.asm
@@ -50,19 +50,28 @@ section .data
     align 16
     packed_decade_mul dw 1, 10, 100, 1000, 1, 10, 100, 1000
     
-    test_str        db "PX 5678 123 debeaf", 0
+    test_str        db "PX 42 233 debeaf", 0
     test_str_len    equ $ - test_str
     
     color_mask      dq 0xffffffffffff
+    
+    big_number      dq 1000000000
 
 section .text
 global _start
 
 _start:
-    mov rsi, test_str
-    lea rdi, [test_str_len]
-    call _parse_msg2
-
+    mov r9, 0
+    mov r10, qword [big_number]
+    .loop:
+        mov rsi, test_str
+        lea rdi, [test_str_len]
+        call _parse_msg2
+    inc r9
+    cmp r9, r10
+    jl .loop
+    mov rax, 60
+    syscall
 
 
 
@@ -71,61 +80,81 @@ _start:
 
 
 _parse_msg2: ;rsi = char* data, rdi = size_t pos
-    movdqu      xmm0, [rsi + rdi - 17]
-    movdqa      xmm1, oword [sixteen_spaces]
+    movdqu      xmm0, [rsi + rdi - 17]          ;load the string into xmm0
+    movdqa      xmm1, oword [sixteen_spaces]    ;move spaces for comparison into register
     vpcmpeqb    xmm2, xmm0, xmm1                ;find spaces
     vpmovmskb   rax, xmm2                       ;mov bitmask to rax
-    
-    ;mov         rbx, rax                   ;copy bitmask to rbx
-    ;shr         rbx, 6
-    ;and         rbx, 1
-    
-    ;jz          .error                     ;bad data (no space before the color
+                                                ;rax now contains a "1 bit" for each byte which was a space
+                                                ;and a "0 bit" for each byte which was not
     
     
     movq        rcx, xmm0                       ;copy color to rcx
     and         rcx, color_mask                 ;select only the color bytes
     
-    shl         ax, 7                          ;shift bitmask
-    or          ax, 0x40                       ;insert marker
-    xor         ebx, ebx
-    lzcnt       bx, ax                        ;get number of Y digits
-    
-    inc         bx                             ;temporary increment for shifting
-    shlx        eax, eax, ebx
+    shl         ax, 7                           ;shift bitmask to find the position of the first relevant space
+    or          ax, 0x40                        ;insert marker for further shifting.
+                                                ;this is needed as we'd actually want to insert 1s from the right
+                                                ;to know where the original lowest bit was
+                                                ;sadly this isnt possible, so we just insert a marker bit
+                                                ;at the position that was the -1st bit before shifting
 
-    lzcnt       ax, ax                        ;get number of X digits
+    xor         ebx, ebx                        ;empty ebx 
+    lzcnt       bx, ax                          ;get number of Y digits
+    
+    inc         bx                              ;temporary increment for shifting as we need to shift
+                                                ;one further than the position of the space to shift the
+                                                ;"1 bit" out of the register
+                                                
+                                                
+    shlx        eax, eax, ebx                   ;perform the variable shift depending on the number of Y digits
+
+    lzcnt       ax, ax                          ;get number of X digits
+                                                ;the number of zeros up to the marker, we originally inserted
+                                                ;is the number of X digits
     
     ;dec         bx                             ;undo increment
+    
+                                                ;for indexing the mask table we need to calculate
+                                                ; 4*(4 - no_Xdigits) + (4 - no_Ydigits)
+                                                ;we do this by negating and adding, but since we incremented bx earlier
+                                                ;we now have to subtract from 5 and not 4
     neg         ax
     neg         bx
     add         ax, 4         
     add         bx, 5         
     
-    shl         eax, 2
+    shl         eax, 2                          ;multiply (4-no_Xdigits) by 4
     
     or          rax, rbx                        ;index for mask table
     
     and         rax, 0xf                        ;make sure we dont index out of range
     
-    shl         rax, 4
+    shl         rax, 4                          ;TODO: lea?
+                                                ;for indexint we need to multiply by 16 
+                                                ;as each row in our LUT is 16 bytes
     
     movdqa      xmm3, oword [mask_0 + rax]      ;get correct mask
     vpshufb     xmm0, xmm0, xmm3                ;extract X and Y
+                                                ;X and Y are now filling a 16 bit int for each digit
+                                                ;whereas they used to only fill 8bits
+                                                ;the reason is that we need to multiply one of each numbers digits
+                                                ;by 1000 so the result would overflow.
+                                                
     
-    movdqa      xmm1, oword [zmask_0 + rax]
+    movdqa      xmm1, oword [zmask_0 + rax]     ;get a mask of zero chars
     vpsubb      xmm0, xmm0, xmm1                ;xmm0 now contains 0X0X0X0X0Y0Y0Y0Y (already subtracted '0' from all of them)
     
     vpmullw     xmm0, xmm0, [packed_decade_mul] ;multiply by 1, 10, 100, 1000 etc
     
-    vphaddw     xmm0, xmm0, xmm0
-    vphaddw     xmm0, xmm0, xmm0                ;add horizontally
+    vphaddw     xmm0, xmm0, xmm0                ;first horizontal reduction to get X0 + X1, X2 + X3, Y0 + Y1, Y2 + Y3
+    vphaddw     xmm0, xmm0, xmm0                ;second horizontal reduction we now have X and Y
+                                                ;in the lower two 16 bit words of xmm0
     
-    movq        rax, xmm0
-    mov         ebx, eax
-    shr         ebx, 16
-    and         ebx, 0xffff
-    and         eax, 0xffff
+    movq        rax, xmm0                       ;copy X and Y to rax
+    mov         ebx, eax                        ;copy to ebx
+    shr         ebx, 16                         ;extract the high word
+    and         ebx, 0xffff                     ;mask off Y
+    and         eax, 0xffff                     ;mask off X
     
     
     ret
